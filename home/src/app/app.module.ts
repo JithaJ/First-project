d7c9d3ac040fb9a54991e72f5e5c04a0b459bffb
jithaj <jithajoseph25@gmail.com>
2020-06-03 17:01:45 +0530

    initial commit

---
+++ home/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewComponent } from './new/new.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+    NewComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
